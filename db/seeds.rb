6397281400ff8250dad452e9bf72755073120782
Charlie Anderson <charliepjanderson@gmail.com>
2019-11-29 11:50:17 +0000

    Adds seeds file, Owner method save passes tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,25 @@
-require_relative('sql_runner.rb')
+require('pry')
+require_relative('../models/animal.rb')
+require_relative('../models/owner.rb')
+
+owner1_details = {
+	"name" => "Charlie Anderson",
+	"address" => "1 Fake Street",
+	"preference" => "all"
+}
+
+owner2_details = {
+	"name" => "Thomas Jenkins",
+	"address" => "2 Fake Street",
+	"preference" => "all"
+}
+
+owner1 = Owner.new(owner1_details)
+owner2 = Owner.new(owner2_details)
+
+owner1.save()
+owner2.save()
+
+binding.pry
+
+nil
