dd9924fb632fb089480150aaa0f4b671b11915d7
Charlie Anderson <charliepjanderson@gmail.com>
2019-12-01 20:23:43 +0000

    Adds training table and sets default owner_id in animals to 1

--- db/shelter.sql
+++ db/shelter.sql
@@ -14,7 +14,19 @@ CREATE TABLE animals (
 	breed VARCHAR,
 	trained BOOLEAN,
 	admission_date DATE,
-	owner_id INT REFERENCES owners(owner_id)
+	owner_id INT DEFAULT 1 REFERENCES owners(owner_id) ON DELETE SET DEFAULT
+);
+
+CREATE TABLE training (
+	training_plan_id SERIAL PRIMARY KEY,
+	toilet_trained BOOLEAN DEFAULT false,
+	sit BOOLEAN DEFAULT false,
+	stay BOOLEAN DEFAULT false,
+	come BOOLEAN DEFAULT false,
+	heel BOOLEAN DEFAULT false,
+	down BOOLEAN DEFAULT false,
+	socialised BOOLEAN DEFAULT false,
+	animal_id INT REFERENCES animals(animal_id) ON DELETE CASCADE
 );
 
 INSERT INTO owners (name, address, preference) VALUES ('Shelter', 'Shelter Address', 'All');
