074300ddbb4891d5c50793e96b096ccf3236de52
Charlie Anderson <charliepjanderson@gmail.com>
2019-12-01 17:17:44 +0000

    Adds new view to Owners

--- controllers/owner_controller.rb
+++ controllers/owner_controller.rb
@@ -8,3 +8,45 @@ get '/volunteer/owner/index' do
 	@pets = Owner.all_pets()
 	erb(:"owners/index")
 end
+
+
+# NEW
+
+get '/volunteer/owner/new' do
+	erb(:"owners/new")
+end
+
+# CREATE
+
+# post '/volunteer/owner/index' do
+# 	new_owner = Owner.new(params)
+# 	new_owner.save()
+# end
+
+# SHOW
+
+get '/volunteer/owner/:id' do
+
+end
+
+# EDIT
+
+get '/volunteer/owner/:id/edit' do
+
+end
+
+# UPDATE
+
+post '/volunteer/owner/:id' do
+
+end
+
+# DESTROY
+
+get '/volunteer/owner/:id/delete' do
+
+end
+
+delete '/volunteer/owner/index' do
+
+end
