e7ae0927912044cfced3c97e34aecb6c28da8ed5
Charlie Anderson <charliepjanderson@gmail.com>
2019-12-01 19:27:16 +0000

    Adds a filter/sort functionality to adopt view, sort by does not work

--- controllers/animal_controller.rb
+++ controllers/animal_controller.rb
@@ -73,37 +73,41 @@ end
 
 # PUBLIC VIEW INDEX SORT BY
 post '/adopt' do
-	filter_type = params[:type]
+	p params
+	values = []
+	sql_array = ["SELECT * FROM animals"]
 
-	# type = dog, trained = yes, sort = name
-	# SELECT * from animals WHERE breed = 'Dog' AND trained = true ORDER BY name;
+	if params[:type].empty? == false
+		values.push(params[:type])
+		where_type = "WHERE breed = $1"
+		sql_array.push(where_type)
+	end
+
+	if params[:trained].empty? == false
+		if params[:trained] == 'yes'
+			values.push("true")
+		else
+			values.push("false")
+		end
+		if params[:type].empty? == false
+			where_trained = "AND trained = $2"
+		else
+			where_trained = "WHERE trained = $1"
+		end
+		sql_array.push(where_trained)
+	end
+
+	if params[:sort].empty? == false
+		values.push(params[:sort])
+		order_by = "ORDER BY $#{values.count()}"
+		sql_array.push(order_by)
+	end
 
 	# TO DO: Need to figure out how to change string to table Name
-
-	if filter_type.length > 0 && params[:trained].length > 0 && params[:sort].length > 0
-		if params[:trained] == 'yes'
-			filter_trained = true
-		elsif params[:trained] == 'no'
-			filter_trained = false
-		end
-
-		case params[:sort]
-		when '1'
-			filter_sort = "type"
-		when '2'
-			filter_sort = "name"
-		when '3'
-			filter_sort = "trained"
-		when '4'
-			filter_sort = "admission_date"
-		end
-
-		sql = "SELECT * FROM animals WHERE breed = $1 AND trained = $2 ORDER BY $3"
-		p "SELECT * FROM animals WHERE breed = #{filter_type} AND trained = #{filter_trained} ORDER BY #{filter_sort}"
-		values = [filter_type, filter_trained, filter_sort]
-		results = SqlRunner.run(sql, values)
-		@pets = results.map{|animal| Animal.new(animal)}
-	end
+	sql = sql_array.join(" ")
+	p sql
+	results = SqlRunner.run(sql, values)
+	@pets = results.map{|animal| Animal.new(animal)}
 
 	erb(:"animals/adopt")
 end
