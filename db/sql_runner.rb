517cf6734595eb24259e9d79e47822c743fe9fd9
Charlie Anderson <charliepjanderson@gmail.com>
2020-01-22 18:54:22 +0000

    getting ready for deployment

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,7 @@ class SqlRunner
 
 	def self.run(sql, values=[])
 		begin
-			db = PG.connect({dbname: 'shelter', host: 'localhost'})
+			db = PG.connect({dbname: 'shelter', host: ENV['DATABASE_HOST']})
 			db.prepare('query', sql)
 			result = db.exec_prepared('query', values)
 		ensure
