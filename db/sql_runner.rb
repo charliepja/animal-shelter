485c293ff1db73525b9748f841a4c97a94dcc6e6
Charlie Anderson <charliepjanderson@gmail.com>
2020-01-27 13:03:26 +0000

    deployment

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,7 @@ class SqlRunner
 
 	def self.run(sql, values=[])
 		begin
-			db = PG.connect({dbname: 'dbr8vksif30q3k', host: 'ec2-107-20-185-16.compute-1.amazonaws.com', port: '5432', user: 'dzzdjgztpnthvy', password: '413d2bd824bfcc294ad7d4ad6e799600e5a33d8c5a253f47d4bab66ac1990c78'})
+			db = PG.connect(ENV["DATABASE_URL"])
 			db.prepare('query', sql)
 			result = db.exec_prepared('query', values)
 		ensure
